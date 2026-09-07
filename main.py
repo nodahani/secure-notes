d5d23e8a45f81dca6b17b3dceee5b32a7116fec6
nodahani <hu3en.home@gmail.com>
2026-09-07 08:09:52 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# UI
+while True:
+    print("1. New Note Safe")
+    print("2. Show Notes")
+    print("3. Edit Note")
+    print("4. Delete note")
+    print("5. Exit!")
+
+    choise = input("What do you want to do?")
+
+    if choise == "5":
+        print("Goodbay!")
+        break
