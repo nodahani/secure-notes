b9e3e68cb2a241becb3d7f5059cf5b9fcd831aa7
nodahani <hu3en.home@gmail.com>
2026-09-09 12:14:40 +0330

    feat: add note editing with multiline input

--- main.py
+++ main.py
@@ -11,18 +11,48 @@ def create_note(name_note):
 def get_note_list():
     files = os.listdir("notes")
     list_note = []
-    for filename in files:
-        if filename.endswith(".txt"):
-            list_note.append(filename[:-4])
+    for fileName in files:
+        if fileName.endswith(".txt"):
+            list_note.append(fileName[:-4])
     return list_note
 
 
 def display_note(note_names):
     index = int(input("Index note... "))
-    for i, filename in enumerate(note_names, start=1):
+    for i, fileName in enumerate(note_names, start=1):
         if index == i:
-            with open("./notes/" + filename + ".txt", "r") as file:
+            with open("./notes/" + fileName + ".txt", "r") as file:
                 return file.read()
+
+
+def get_multiline_input(file):
+    lines = []
+    for count, line in enumerate(file, start=1):
+        # Display note line by line
+        print(f"{count}. {line}")
+
+        edit_line_note = input("If you want to edit, type, otherwise press Enter... :")
+        if edit_line_note != "":
+            lines.append(edit_line_note + "\n")
+        else:
+            lines.append(line)
+    return lines
+
+
+def edit_note():
+    fileNames = get_note_list()
+    for i, fileName in enumerate(fileNames, start=1):
+        print(f"{i}. {fileName}")
+
+    index = int(input("Index note... "))
+    for i, fileName in enumerate(fileNames, start=1):
+        if index == i:
+
+            with open("./notes/" + fileName + ".txt", "r") as file:
+                new_note = get_multiline_input(file)
+
+            with open("./notes/" + fileName + ".txt", "w") as file:
+                file.writelines(new_note)
 
 
 # UI
@@ -51,6 +81,9 @@ while True:
 
         selected_note = display_note(notes)
         print(f"...\n{selected_note}\n...")
+
+    if choise == "3":
+        print(edit_note())
 
     if choise == "5":
         print("Goodbay!")
