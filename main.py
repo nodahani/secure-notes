6b6b8ccbca067e2e45eedf8980af9a1f2f719ef1
nodahani <hu3en.home@gmail.com>
2026-09-15 09:09:27 +0330

    refactor: rename note references to file in UI

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import constants
 
 # UI
 while True:
-    print("1. New Note Safe")
-    # print("2. Show Notes")
-    # print("3. Edit Note")
-    # print("4. Delete note")
-    print("5. Exit!")
+    print("1. Create New File")
+    print("2. View Files")
+    # print("3. Edit File")
+    # print("4. Delete File")
+    print("5. Exit")
 
     choise = input("What do you want to do...? ")
 
